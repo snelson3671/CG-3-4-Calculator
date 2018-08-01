67981627b1b4c2ddb3c8bdc5d617ad414dac55b5
Sandra <snelson3671@yahoo.com>
2018-08-01 18:24:27 -0500

    CG 3-4 Calculator

--- CG 3-4 Calculator/Program.cs
+++ CG 3-4 Calculator/Program.cs
@@ -6,7 +6,41 @@ namespace CG_3_4_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sandra's calculator");
+            
+            Console.Write("Please type the operation you would like to perform. (+, -, *, or /)   ");
+            string operation = Console.ReadLine();
+
+            Console.Write("Now type the first number.  ");
+            string x = Console.ReadLine();
+
+            Console.Write("Now type the second number.  ");
+            string y = Console.ReadLine();
+            decimal c = 0;
+
+            
+            decimal a = Convert.ToDecimal(x);
+            decimal b = Convert.ToDecimal(y);
+            
+            
+            if (operation == "+")
+                c = a + b;
+            else if (operation == "-")
+                c = a - b;
+            else if (operation == "*")
+                c = a * b;
+            else if (operation == "/")
+                c = a / b;
+            else
+                Console.WriteLine("I don't understand.  Please try again.");
+
+            Console.WriteLine($"Your answer is {c}" );
+            Console.ReadLine();
+           
+               
+
+
+            
         }
     }
 }
